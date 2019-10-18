b35dc04b58119508464032dc9ee99d52d5ef39c1
Danny Laptop <danny.ly.cs@outlook.com>
2019-10-18 02:00:58 -0700

    Added MemberService to start up, Exposes MemberService to public

--- gRPCServer/gRPCServer/Startup.cs
+++ gRPCServer/gRPCServer/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using gRPCServer.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -32,6 +33,7 @@ namespace gRPCServer
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<MemberService>();
 
                 endpoints.MapGet("/", async context =>
                 {
