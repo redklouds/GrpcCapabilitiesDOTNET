a98155521e63473fe8e15628af893f1c52573385
Danny Laptop <danny.ly.cs@outlook.com>
2019-10-18 00:55:18 -0700

    Added common models project, class library, and added funtion to return member data

--- gRPCServer/RESTServer/Controllers/MemberDataController.cs
+++ gRPCServer/RESTServer/Controllers/MemberDataController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Common.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -26,5 +27,20 @@ namespace RESTServer.Controllers
         {
             return Ok("This is member Index");
         }
+    
+
+        [HttpPost]
+        [Route("GetMemberData")]
+        public ActionResult getMemberData([FromBody] DataRequst dataRequest)
+        {
+            //returns a single member data given the member ID
+            if(dataRequest == null)
+            {
+                return BadRequest("Error parsing Data Request");
+            }
+
+        }
     }
+
+
 }
