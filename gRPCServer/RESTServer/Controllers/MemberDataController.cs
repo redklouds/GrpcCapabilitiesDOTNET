365d82f2500224f595355bd73dcdeddb5c2cbe78
Danny Laptop <danny.ly.cs@outlook.com>
2019-10-18 00:45:56 -0700

    Add files

---
+++ gRPCServer/RESTServer/Controllers/MemberDataController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RESTServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberDataController : ControllerBase
+    {
+        private readonly ILogger<MemberDataController> _logger;
+
+        public MemberDataController(ILogger<MemberDataController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        [Route("MemberIndex")]
+        public ActionResult tester()
+        {
+            return Ok("YOLO SWAGGER");
+        }
+    }
+}
