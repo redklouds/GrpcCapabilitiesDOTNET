e4f520361ab1ae94e61ecbd77716b40ad02dded2
Danny Laptop <danny.ly.cs@outlook.com>
2019-10-18 00:49:02 -0700

    added member index response

--- gRPCServer/RESTServer/Controllers/MemberDataController.cs
+++ gRPCServer/RESTServer/Controllers/MemberDataController.cs
@@ -24,7 +24,7 @@ namespace RESTServer.Controllers
         [Route("MemberIndex")]
         public ActionResult tester()
         {
-            return Ok("YOLO SWAGGER");
+            return Ok("This is member Index");
         }
     }
 }
