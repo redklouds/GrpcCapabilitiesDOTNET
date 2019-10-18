a0ae01978ba30dde0a2b5eff0a599f656599ab90
Danny Laptop <danny.ly.cs@outlook.com>
2019-10-18 01:13:36 -0700

    added member Model and finished mock data for member

---
+++ gRPCServer/Common.Models/Member.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Models
+{
+    public class Member
+    {
+
+        public string MemberID { get; set; }
+
+        public string SSN { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+    }
+}
--- gRPCServer/Repository/MockDataRepository.cs
+++ gRPCServer/Repository/MockDataRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Common.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,7 +13,24 @@ namespace Repository
 
         public DataResponse GetMemberData(string memberId)
         {
+            Member testMbr = new Member
+            {
+                FirstName = "Dante",
+                LastName = "Ly",
+                City = "Seattle",
+                MemberID = "69696969",
+                SSN = "242342"
+            };
 
+            //TODO other things such as getting data from somewhere else lol
+
+            DataResponse dr = new DataResponse
+            {
+                ResponseBody = JsonConvert.SerializeObject(testMbr),
+                StatusCode = 200
+
+            };
+            return dr;
         }
     }
 }
