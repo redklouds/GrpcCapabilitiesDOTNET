3a7f7fb586269f87c9e0fc06447c48355fd03c96
Danny Laptop <danny.ly.cs@outlook.com>
2019-10-19 09:25:40 -0700

    added unit test

--- gRPCServer/RESTServer.Tests/UnitTest1.cs
+++ gRPCServer/RESTServer.Tests/UnitTest1.cs
@@ -1,18 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RESTServer.Controllers;
+using System.Net;
+
 namespace RESTServer.Tests
 {
     [TestClass]
     public class UnitTest1
     {
+        private ILogger<MemberDataController> log;
+
         [TestMethod]
         public void TestMethod1()
         {
 
-            ILogger log;
-            var controller = new MemberDataController();
 
+            var controller = new MemberDataController(log);
+
+            ActionResult  result = controller.getMemberData(new Common.Models.DataRequst
+            {
+                MemberID = null
+            });
+            Assert.AreEqual(result, typeof(BadRequestResult));
         }
     }
 }
