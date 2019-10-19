380e70e66f24c008a4f3be8704327bad04b56b43
Danny Laptop <danny.ly.cs@outlook.com>
2019-10-18 18:42:49 -0700

    added unit test

---
+++ gRPCServer/RESTServer.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RESTServer.Controllers;
+namespace RESTServer.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            ILogger log;
+            var controller = new MemberDataController();
+
+        }
+    }
+}
---
+++ gRPCServer/gRPCServer.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using gRPCServer;
+
+namespace gRPCServer.Tests
+{
+    public class Tests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+            
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
